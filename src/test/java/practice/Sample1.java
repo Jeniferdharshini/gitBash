1dfac64d5479d14ccb3a205e9b7a8098f7c6ec8a
jenidharshini <jenidharshini@gmail.com>
2023-03-28 14:13:09 -0500

    committed

---
+++ src/test/java/practice/Sample1.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+	@Test
+	public void test() {
+		System.out.println("Test running");
+             System.out.println("pull back");
+
+	}
+
+}
+
+
+//push back again
